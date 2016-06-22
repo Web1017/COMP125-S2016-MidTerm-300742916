bdcb83e2e7a7c150ffcf484ef2ac2e3471cb03b7
jniftali <300742916@cencol.ca>
2016-06-22 13:00:32 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# COMP125-MidTerm - 300742916
+
+MidTerm Exam Template for COMP125 - Client-Side Web Development @ Centennial College
---
+++ Scripts/app.js
@@ -0,0 +1,29 @@
+/*
+*
+*
+*
+*/
+
+// setup your IIFE (Immediately Invoked Function Expression)
+(function () {
+"use strict";
+var paragraphText = [];
+paragraphText[0] = document.getElementById("paragraphOne");
+paragraphText[1] = document.getElementById("paragraphTwo");
+paragraphText[2] = document.getElementById("paragraphThree");
+paragraphText[3] = document.getElementById("paragraphFour");
+paragraphText[4] = document.getElementById("paragraphFive");
+paragraphText[5] = document.getElementById("paragraphSix");
+paragraphText[6] = document.getElementById("paragraphSeven");
+paragraphText[7] = document.getElementById("paragraphEight");
+paragraphText[8] = document.getElementById("paragraphNine");
+paragraphText[9] = document.getElementById("paragraphTen");
+
+ paragraphs[0] = "
+
+
+
+
+
+})();
+
